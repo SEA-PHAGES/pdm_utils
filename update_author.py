9a214fb79f4db328b3499df45fbc725954c99207
Travis Mavrich <trm53@pitt.edu>
2017-05-06 09:52:07 -0400

    created update author script

---
+++ update_author.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+#PYTHON code for updating annotation authorship data from CSV
+#Travis Mavrich
+#Modified scripts written by Cbowman
+
+
+import MySQLdb as mdb
+import sys
+
+#Get the parameters
+try:
+	database = sys.argv[1]
+	infile = sys.argv[2]
+except:
+	print "Incorrect Parameters - ./update_phageid.py DATABASE AUTHOR_CSV"
+	sys.exit(1)
+
+#Get the gene information
+try:
+
+	f = open(infile,'r')
+	lines= f.read().splitlines()
+	f.close()
+
+	#Connect
+	con = mdb.connect('localhost', 'root', 'phage', database)
+	cur = con.cursor()
+
+	for line in lines:
+		line = line.split(',')
+		print "update phage set AnnotationAuthor = '" + str(line[1]) + "' where PhageID = '" + str(line[0]) + "';"
+		cur.execute("update phage set AnnotationAuthor = '" + str(line[1]) + "' where PhageID = '" + str(line[0]) + "';")
+
+	cur.execute("COMMIT")
+	con.close()
+
+except mdb.Error, e:
+
+	print "Error %d: %s" % (e.args[0],e.args[1])
+	sys.exit(1)update
