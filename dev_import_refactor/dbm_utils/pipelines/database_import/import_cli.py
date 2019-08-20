123179defbbca1fd0b65e84e42bfbbf57676c267
Travis Mavrich <trm53@pitt.edu>
2019-08-20 15:24:10 -0400

    added gitignore

--- dev_import_refactor/dbm_utils/pipelines/database_import/import_cli.py
+++ dev_import_refactor/dbm_utils/pipelines/database_import/import_cli.py
@@ -2,142 +2,132 @@
 UNIX command line interface so that the import process provides
 interactive feedback about the import process.
 """
-
-
-
 # Built-in libraries
-import time, sys, os, getpass, csv, re, shutil
-import json, urllib
+import time
+import sys
+import os
+import getpass
+import csv
+import re
+import shutil
+import json
+import urllib
+import argparse
 from datetime import datetime
-from classes import Genome
-
-from functions import phamerator
-from functions import flat_files
-from functions import tickets
-from classes import MySQLConnectionHandler
-
-
 # Import third-party modules
 try:
     pass
 except ModuleNotFoundError as err:
     print(err)
-	sys.exit(1)
-
-
-script_description = """
-    This is a script to import and update phage genomes in
-    the Phamerator database.
-    It requires three arguments:
-    First argument: name of MySQL database that will be updated
-        (e.g. 'Actino_Draft').
-    Second argument: directory path to the folder of genome files that
-        will be uploaded (genbank-formatted).
-    Third argument: directory path to the import table file with the
-        following columns (csv-formatted):
-
-        1. Action to implement on the database (add, remove, replace, update)
-        2. PhageID to add or update
-        3. Host genus of the updated phage
-        4. Cluster of the updated phage
-        5. Subcluster of the updated phage
-        6. Annotation status of the updated phage (draft, final, gbk)
-        7. Annotation authorship of the updated phage (hatfull, gbk)
-        8. Gene description field of the updated phage (product, note, function)
-        9. Accession of the updated phage
-        10. Run mode of the updated phage
-        11. PhageID that will be removed or replaced
-"""
-
-
-
-# TODO re-structure using argparse.
-# TODO confirm arguments are structured properly.
-# Get the command line parameters.
-try:
-    database = sys.argv[1]
-    genome_folder = sys.argv[2]
-    import_table = sys.argv[3]
-except:
-    print(script_description)
-	sys.exit(1)
-
-
-# Confirm that genome folder exists.
-genome_folder = basic.expand_path(genome_folder)
-
-
-# TODO not sure if this is needed anymore.
-# #Add '/' at the end if it's not there
-# if genome_folder[-1] != "/":
-#     genome_folder = genome_folder + "/"
-
-
-if not basic.verify_path(genome_folder, "dir"):
-    print "\n\nInvalid input for genome folder.\n\n"
     sys.exit(1)
 
+# Package modules
+from functions import phamerator
+from functions import flat_files
+from functions import tickets
+from functions import basic
+from classes import MySQLConnectionHandler
+from classes import Genome
+from constants import constants
 
-# Confirm that import table exists.
-import_table = basic.expand_path(import_table)
-if not basic.verify_path(import_table, "file"):
-    print "\n\nInvalid input for import table file.\n\n"
+#
+# script_description = """
+#     Structure of import ticket table:
+#         following columns (csv-formatted):
+#         1. Action to implement on the database (add, remove, replace, update)
+#         2. PhageID to add or update
+#         3. Host genus of the updated phage
+#         4. Cluster of the updated phage
+#         5. Subcluster of the updated phage
+#         6. Annotation status of the updated phage (draft, final, gbk)
+#         7. Annotation authorship of the updated phage (hatfull, gbk)
+#         8. Gene description field of the updated phage (product, note, function)
+#         9. Accession of the updated phage
+#         10. Run mode of the updated phage
+#         11. PhageID that will be removed or replaced
+#     """
+
+parser = argparse.ArgumentParser(
+    description="Import genomes into a Phamerator database.")
+parser.add_argument("-g", "--genome_folder", type=os.path.abspath,
+    required=True,
+    help=("Path to the folder containing GenBank-formatted "
+          "flat files to be processed."))
+parser.add_argument("-t", "--table", type=os.path.abspath, required=True,
+    help="Path to the CSV-formatted 12-field table containing instructions "
+         "to process each genome. ")
+parser.add_argument("-db", "--database", type=str, default=False,
+    help="Name of the MySQL database to import the genomes.")
+parser.add_argument("-f", "--filename", action="store_true", default=False,
+    help="Indicates whether the filename should be used to identify the genome.")
+args = parser.parse_args()
+
+# Confirm that genome folder and import table exists.
+if not basic.verify_path(args.genome_folder, "dir"):
+    print("\n\nInvalid input for genome folder.\n\n")
+    sys.exit(1)
+if not basic.verify_path(args.table, "file"):
+    print("\n\nInvalid input for import table file.\n\n")
     sys.exit(1)
 
-
-# TODO replace with new function?
 # Create output directories
 date = time.strftime("%Y%m%d")
-
+attempts = 3
 failed_folder = '%s_failed_upload_files' % date
 success_folder = '%s_successful_upload_files' % date
-
-try:
-    os.mkdir(os.path.join(genome_folder,failed_folder))
-except:
-    print "\nUnable to create output folder: %s" % \
-        os.path.join(genome_folder,failed_folder)
+f_valid = False
+f_count = 0
+while not f_valid and f_count < attempts:
+    if f_count > 0:
+        failed_folder_mod += "_" + str(f_count)
+    failed_dir = os.path.join(args.genome_folder,failed_folder)
+    if not basic.verify_path(failed_dir, "dir"):
+        f_valid = True
+        os.mkdir(failed_dir)
+    f_count += 1
+if not f_valid:
+    print("\nUnable to create failed_folder")
+    sys.exit(1)
+success_folder = '%s_failed_upload_files' % date
+success_folder = '%s_successful_upload_files' % date
+success_folder_valid = False
+success_folder_count = 0
+while not success_folder_valid and success_folder_count < attempts:
+    if success_folder_count > 0:
+        success_folder += "_" + str(success_folder_count)
+    failed_dir = os.path.join(args.genome_folder,success_folder)
+    if not basic.verify_path(failed_dir, "dir"):
+        success_folder_valid = True
+        os.mkdir(failed_dir)
+    success_folder_count += 1
+if not success_folder_valid:
+    print("\nUnable to create success_folder")
     sys.exit(1)
 
 
-try:
-    os.mkdir(os.path.join(genome_folder,success_folder))
-except:
-    print "\nUnable to create output folder: %s" % \
-        os.path.join(genome_folder,success_folder)
-    sys.exit(1)
-
-
-
-
-
-# TODO command now should include an argument that specifies id_field
-# from which id should be assigned as flat files are parsed.
-
-
-
-
-
-
-# TODO create SQL connector object using parsed arguments.
-# TODO add sql connection option to script arguments.
-if sql_argument:
+# TODO add sql connection option to script arguments?
+if args.database is not False:
     sql_handle = MySQLConnectionHandler.MySQLConnectionHandler()
-    sql_handle.username = "" # Populate from script arguments.
-    sql_handle.password = "" # Populate from script arguments.
-    sql_handle.database = "" # Populate from script arguments.
+    sql_handle.username = getpass.getpass(prompt="Provide the MySQL username: ")
+    sql_handle.password = getpass.getpass(prompt="Provide the MySQL password: ")
+    sql_handle.database = args.database
+
+    # TODO test the sql connection. Verify that the user-provided parameters
+    # are correct. Otherwise, exit the script.
+    sql_handle.validate_credentials()
+    sql_handle.validate_database_access()
+    if (not sql_handle.credential_status or not sql_handle._database_status):
+        print("\nUnable to connect to the database")
+        sys.exit(1)
 else:
     sql_handle = None
 
 
 
-# TODO test the sql connection. Verify that the user-provided parameters
-# are correct. Otherwise, exit the script.
 
 
 # Identify valid files in folder for evaluation.
-files_in_folder = basic.identify_files(genome_dir)
-
+files_in_folder = basic.identify_files(args.genome_folder)
 
 
 # TODO match record to ticket in bundle object.
@@ -154,9 +144,11 @@ files_in_folder = basic.identify_files(genome_dir)
 
 
 # Retrieve import ticket data.
-# lists_of_ticket_data = read.csv(ticket_filename)
-lists_of_ticket_data # TODO run a function to read the import table and retrieve the data.
-
+lists_of_ticket_data = []
+with open(args.table,'r') as file:
+    file_reader = csv.reader(file)
+    for row in file_reader:
+        lists_of_ticket_data.append(row)
 
 
 
