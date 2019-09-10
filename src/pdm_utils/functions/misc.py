e535c7cf5efa41e15d14b0e1d1c32101d680b81c
Travis Mavrich <trm53@pitt.edu>
2019-09-09 22:25:16 -0400

    formatted misc function docstrings for sphinx

--- src/pdm_utils/functions/misc.py
+++ src/pdm_utils/functions/misc.py
@@ -1,30 +1,32 @@
 """Misc. functions useful for comparing and processing genomes."""
-
-
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 from Bio.Alphabet import IUPAC
 
 
-
-
-
-def match_genome_by_id(bundle_obj, genome_dict, key1, key2 = None):
+def match_genome_by_id(bndl, genome_dict, key1, key2=None):
     """Match genome object to another genome object using id.
-    The 'key1' parameter provides the type of genome stored in the Bundle
-    genome dictionary to base the match from.
-    The 'key2' parameter provides the type of genome to be stored in
-    the Bundle genome dictionary."""
 
+    :param bndl:
+        A bundle object containing at least two pdm_utils genome objects.
+    :type bndl: pdm_utils bundle
+    :param key1:
+        Indicates the type of genome stored in the bundle
+        genome dictionary to base the match from.
+    :type key1: str
+    :param key2:
+        Indicates the type of genome to be stored in
+        the bundle genome dictionary.
+    :type key2: str
+    """
     try:
-        ref_genome = bundle_obj.genome_dict[key1]
+        ref_genome = bndl.genome_dict[key1]
         if ref_genome.id in genome_dict.keys():
             matched_genome = genome_dict[ref_genome.id]
-
             if key2 is None:
-                bundle_obj.genome_dict[matched_genome.type] = matched_genome
+                bndl.genome_dict[matched_genome.type] = matched_genome
             else:
-                bundle_obj.genome_dict[key2] = matched_genome
+                bndl.genome_dict[key2] = matched_genome
     except:
         pass
 
@@ -32,42 +34,50 @@ def match_genome_by_id(bundle_obj, genome_dict, key1, key2 = None):
 
 
 # TODO this is probably not needed anymore.
-def match_genomes(list_of_bundle_objects, genome_dict, key1, key2 = None):
+def match_genomes(bndl_list, genome_dict, key1, key2=None):
     """Match genome object to another genome object using id.
-    The 'key1' parameter provides the type of genome stored in the Bundle
-    genome dictionary to base the match from.
-    The 'key2' parameter provides the type of genome to be stored in
-    the Bundle genome dictionary."""
 
+    :param bndl_list: List of bundle objects.
+    :type bndl_list: list
+    :param genome_dict: A dictionary of pdm_utils genome objects.
+    :type genome_dict: dict
+    :param key1:
+        Indicates the type of genome stored in the Bundle
+        genome dictionary to base the match from.
+    :type key1: str
+    :param key2:
+        Indicates the type of genome to be stored in
+        the bundle genome dictionary.
+    :type key2: str
+    """
     index = 0
-    while index < len(list_of_bundle_objects):
-
-        bundle_obj = list_of_bundle_objects[index]
-
+    while index < len(bndl_list):
+        bndl = bndl_list[index]
         try:
-            ref_genome = bundle_obj.genome_dict[key1]
+            ref_genome = bndl.genome_dict[key1]
             if ref_genome.id in genome_dict.keys():
                 matched_genome = genome_dict[ref_genome.id]
-
                 if key2 is None:
-                    bundle_obj.genome_dict[matched_genome.type] = matched_genome
+                    bndl.genome_dict[matched_genome.type] = matched_genome
                 else:
-                    bundle_obj.genome_dict[key2] = matched_genome
-
+                    bndl.genome_dict[key2] = matched_genome
         except:
             pass
-
         index += 1
 
 
-
-
 def create_fasta_seqrecord(header, sequence_string):
-    """Create a fasta-formatted Biopython SeqRecord object."""
+    """Create a fasta-formatted Biopython SeqRecord object.
 
+    :param header: Description of the sequence.
+    :type header: str
+    :param sequence_string: Nucleotide sequence.
+    :type sequence_string: str
+    :returns: Biopython SeqRecord containing the nucleotide sequence.
+    :rtype: SeqRecord
+    """
     seq = Seq(sequence_string, alphabet = IUPAC.unambiguous_dna)
     seqrecord = SeqRecord(seq, description = header)
-
     return seqrecord
 
 
