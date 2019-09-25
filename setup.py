e8ab252075321c578245f65d0862273bb2470a0a
Travis Mavrich <trm53@pitt.edu>
2019-09-25 14:34:47 -0400

    updated pypi package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pdm_utils",
-    version="0.0.5",
+    version="0.0.6",
     author="Travis Mavrich",
     author_email="trm53@pitt.edu",
     description="Phamerator database management utilities",
