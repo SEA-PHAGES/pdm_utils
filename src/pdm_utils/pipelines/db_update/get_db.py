5a1dd81a2919786b6514fb6d2ec4f380fb1269da
Travis Mavrich <trm53@pitt.edu>
2019-11-18 11:32:29 -0500

    improved get_db pipeline

--- src/pdm_utils/pipelines/db_update/get_db.py
+++ src/pdm_utils/pipelines/db_update/get_db.py
@@ -9,21 +9,6 @@ from pdm_utils.classes import mysqlconnectionhandler as mch
 from pdm_utils.constants import constants
 from pdm_utils.functions import basic
 
-# TODO not tested, but identical function in import_genome.py tested.
-def setup_sql_handle(database):
-    """Connect to a MySQL database."""
-    sql_handle = mch.MySQLConnectionHandler()
-    sql_handle.database = database
-    sql_handle.open_connection()
-    if (not sql_handle.credential_status or not sql_handle._database_status):
-        print(f"No connection to the {database} database. "
-            f"Valid credentials: {sql_handle.credential_status}. "
-            f"Valid database: {sql_handle._database_status}")
-        sys.exit(1)
-    else:
-        return sql_handle
-
-
 
 # TODO not tested, but identical function in import_genome.py tested.
 def set_path(path, kind=None, expect=True):
@@ -37,7 +22,6 @@ def set_path(path, kind=None, expect=True):
     else:
         return path
 
-# TODO in development
 # TODO unittest.
 def main(unparsed_args_list):
     """Run the get_db pipeline."""
@@ -48,7 +32,10 @@ def main(unparsed_args_list):
         print("The output ")
     # curl website > output_file
     # Version file
-    version_filename = args.database + ".version"
+    if args.filename is None:
+        version_filename = args.database + ".version"
+    else:
+        version_filename = args.filename + ".version"
     version_url = constants.DB_WEBSITE + version_filename
     version_filepath = pathlib.Path(args.output_folder, version_filename)
     if args.download == True:
@@ -65,7 +52,10 @@ def main(unparsed_args_list):
                     print(f"Unable to download {version_filename} from server.")
 
     # Database file
-    db_filename = args.database + ".sql"
+    if args.filename is None:
+        db_filename = args.database + ".sql"
+    else:
+        db_filename = args.filename + ".sql"
     db_url = constants.DB_WEBSITE + db_filename
     db_filepath = pathlib.Path(args.output_folder, db_filename)
     if args.download == True:
@@ -75,6 +65,10 @@ def main(unparsed_args_list):
             command_string2 = f"curl {db_url}"
             command_list2 = command_string2.split(" ")
             status = False
+            # TODO if the file is not found on the server,
+            # a file will still be created with text indicating
+            # there was an error. So this step needs to catch that
+            # error.
             with db_filepath.open("w") as db_handle:
                 try:
                     print("Downloading sql file.")
@@ -84,27 +78,29 @@ def main(unparsed_args_list):
                     print(f"Unable to download {db_filename} from server.")
 
     # Install new database
-    # TODO this should first check if the database exists in MySQL.
-    # If not, it should create the database.
     if args.install == True:
-        result = basic.verify_path2(db_filepath, kind="file", expect=True)
-        if result[0] == False:
-            print("Unable to locate database file to install.")
-            print(result[1])
+        result1 = basic.verify_path2(db_filepath, kind="file", expect=True)
+        if result1[0] == True:
+            sql_handle = mch.MySQLConnectionHandler()
+            sql_handle.open_connection()
+            if sql_handle.credential_status:
+                result2 = create_new_db(sql_handle, args.database)
+                if result2 == 0:
+                    sql_handle.database = args.database
+                    sql_handle.open_connection()
+                    if (sql_handle.credential_status == True and
+                            sql_handle._database_status == True):
+                        install_db(sql_handle, db_filepath)
+                    else:
+                        print(f"No connection to the {args.database} database due "
+                              "to invalid credentials or database.")
+                else:
+                    print("Unable to create new, empty database.")
+            else:
+                print("Invalid MySQL credentials.")
         else:
-            # TODO currently there really is no need to set up a sql_handle object.
-            # But it will be used once the pipeline is improved.
-            sql_handle = setup_sql_handle(args.database)
-            command_string3 = (f"mysql -u {sql_handle.username} "
-                               f"-p{sql_handle.password} {sql_handle.database}")
-            command_list3 = command_string3.split(" ")
-            with db_filepath.open("r") as db_handle:
-                try:
-                    print("Installing database...")
-                    subprocess.check_call(command_list3, stdin=db_handle)
-                    print("Installation complete.")
-                except:
-                    print(f"Unable to install {db_filename} in MySQL.")
+            print("Unable to locate database file for installation.")
+            print(result1[1])
 
     if args.remove == True:
         print("Removing downloaded data.")
@@ -133,10 +129,14 @@ def parse_args(unparsed_args_list):
          "database or create a new database if it is not already present.")
     REMOVE_HELP = \
         ("Indicates if the downloaded file should be removed after installation.")
-
     ALL_HELP = \
         ("Indicates if the entire pipeline should run: "
          "download, install, and then remove the temp files.")
+    FILENAME_HELP = \
+        ("Indicates the name of the SQL file and verion file. "
+         "By default, the filename is "
+         "assumed to be the same as the database name. This option enables "
+         "database to be created from a file of a different name.")
 
     parser = argparse.ArgumentParser(description=UPDATE_DB_HELP)
     parser.add_argument("database", type=str, help=DATABASE_HELP)
@@ -150,6 +150,9 @@ def parse_args(unparsed_args_list):
         default=False, help=REMOVE_HELP)
     parser.add_argument("-a", "--all_steps", action="store_true",
         default=False, help=ALL_HELP)
+    parser.add_argument("-f", "--filename", type=str,
+        help=FILENAME_HELP)
+
 
     # TODO implement this option.
     # parser.add_argument("-f", "--force_update", action="store_true",
@@ -165,5 +168,39 @@ def parse_args(unparsed_args_list):
         args.install = True
         args.remove = True
 
-
     return args
+
+
+# TODO unittest.
+def create_new_db(sql_handle, database):
+    """Creates a new, empty database."""
+    # First, test if a test database already exists within mysql.
+    # If there is, delete it so that a fresh test database is installed.
+    query = ("SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA "
+             f"WHERE SCHEMA_NAME = '{database}'")
+    result1 = sql_handle.execute_query(query)
+    if len(result1) != 0:
+        statement1 = [f"DROP DATABASE {database}"]
+        result2 = sql_handle.execute_transaction(statement1)
+    else:
+        result2 = 0
+    if result2 == 0:
+        # Next, create the database within mysql.
+        statement2 = [f"CREATE DATABASE {database}"]
+        result2 = sql_handle.execute_transaction(statement2)
+        sql_handle.close_connection()
+    return result2
+
+# TODO unittest.
+def install_db(sql_handle, schema_filepath):
+    """Install a MySQL file into the indicated database."""
+    command_string = (f"mysql -u {sql_handle.username} "
+                      f"-p{sql_handle.password} {sql_handle.database}")
+    command_list = command_string.split(" ")
+    with schema_filepath.open("r") as fh:
+        try:
+            print("Installing database...")
+            subprocess.check_call(command_list, stdin=fh)
+            print("Installation complete.")
+        except:
+            print(f"Unable to install {schema_filepath.name} in MySQL.")
