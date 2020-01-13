11c3f0560d44de463faa3020e1d63828e787f151
laa89 <laa89@pitt.edu>
2020-01-13 09:03:14 -0500

    Unittests and Bug Fix

---
+++ tests/integration/test_filter.py
@@ -0,0 +1,11 @@
+"""Integration unittests for the filter module"""
+
+from pdm_utils.classes.filter import Filter
+import pdm_utils.classes.filter
+from unittest.mock import Mock, patch
+import unittest
+
+class TestFilter(unittest.TestCase):
+
+if __name__ == "__main__":
+    unittest.main()
