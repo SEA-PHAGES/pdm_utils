89fd1a39b2f4baad9a626590bb626481eb7d3687
laa89 <laa89@pitt.edu>
2020-01-16 16:34:22 -0500

    SchemaGraph update

---
+++ tests/integration/test_schemagraph.py
@@ -0,0 +1,12 @@
+"""Integration unittests for the schemagraph module"""
+
+from pdm_utils.classes.schemagraph import SchemaGraph
+import pdm_utils.classes.schemagraph
+from unittest.mock import Mock, patch
+import unittest
+
+class TestSchemaGraph(unittest.TestCase):
+    pass
+
+if __name__ == "__main__":
+    unittest.main()
