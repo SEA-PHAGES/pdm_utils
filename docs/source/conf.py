c4742d9480fd71780f10af125ba09db7d9540e51
Travis Mavrich <trm53@pitt.edu>
2020-01-31 15:24:29 -0500

    updated package version to 0.0.19

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,9 +20,9 @@ sys.path.insert(0, os.path.abspath('../../src/'))
 project = 'pdm_utils'
 copyright = '2019, Travis Mavrich'
 author = 'Travis Mavrich'
-version = '0.0.18'
+version = '0.0.19'
 # The full version, including alpha/beta/rc tags
-release = '0.0.18'
+release = '0.0.19'
 
 
 # -- General configuration ---------------------------------------------------
