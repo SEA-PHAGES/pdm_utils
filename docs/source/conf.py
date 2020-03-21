95158ee0c391301a168ff8bf0b453d5178ca70bc
Travis Mavrich <trm53@pitt.edu>
2020-03-21 07:36:36 -0400

    incremented package version to 0.1.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,9 +20,9 @@ sys.path.insert(0, os.path.abspath('../../src/'))
 project = 'pdm_utils'
 copyright = '2019, Travis Mavrich'
 author = 'Travis Mavrich'
-version = '0.1.0'
+version = '0.1.1'
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.1.1'
 
 
 # -- General configuration ---------------------------------------------------
