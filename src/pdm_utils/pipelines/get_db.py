e339cd542c40b2b26b02f77f51fb2f82d147fda6
Travis Mavrich <trm53@pitt.edu>
2020-03-28 23:05:18 -0400

    improved get_db

--- src/pdm_utils/pipelines/get_db.py
+++ src/pdm_utils/pipelines/get_db.py
@@ -15,122 +15,96 @@ DEFAULT_OUTPUT_FOLDER = "/tmp/"
 CURRENT_DATE = date.today().strftime("%Y%m%d")
 RESULTS_FOLDER = f"{CURRENT_DATE}_get_db"
 
-# TODO unittest.
+# TODO test.
 def main(unparsed_args_list):
     """Run the get_db pipeline.
 
     The database data can be retrieved from three places:
-    The server, in which is needs to be downloaded to a new folder.
+    The server, which needs to be downloaded to a new folder.
     A local file, in which no download and no new folder are needed.
     The empty schema stored within pdm_utils, in which no download, new folder,
     or local file are needed.
     """
     args = parse_args(unparsed_args_list)
 
-    # If filename was provided while also specifying to create new database
-    # or download database, throw an error.
-    if args.filename is not None:
-        if (args.download == True or args.new == True):
-            print(f"Conflicting arguments. If installing from local file, "
-                  "do not also select to create a new database or "
-                  "download database from server.")
-            sys.exit(1)
+    # Set all values to get rid of args object and to set additional values.
+    database = args.database
+    option = args.option
+    install = True
+    schema_version = None
+    db_filepath = None
 
-    args.output_folder = basic.set_path(args.output_folder, kind="dir", expect=True)
-
-    if args.filename is not None:
-        args.filename = basic.set_path(args.filename, kind="file", expect=True)
-        results_path = None
-    elif args.new == True:
-        results_path = None
+    if option == "file":
+        db_filepath = basic.set_path(args.filename, kind="file", expect=True)
+    elif option == "new":
+        schema_version = args.schema_version
     else:
-        # If not installing from local file, create new dir to store
-        # downloaded data.
+        # option must be "server"
+        output_folder = basic.set_path(args.output_folder, kind="dir", expect=True)
+        download = True
+        remove = True
         results_folder = pathlib.Path(RESULTS_FOLDER)
-        results_path = basic.make_new_dir(args.output_folder,
-                                          results_folder, attempt=50)
+        results_path = basic.make_new_dir(output_folder, results_folder, attempt=50)
+        if args.download_only == True:
+            install = False
+            remove = False
+
         if results_path is None:
             print("Unable to create results folder.")
             sys.exit(1)
-
-    if args.new == True:
-        schema_version = choose_schema_version()
-    else:
-        schema_version = None
-
-    # curl website > output_file
-    if args.download == True:
-        version_filepath, status1 = get_data(results_path, constants.DB_WEBSITE,
-                                             args.database, "version")
-        db_filepath, status2 = get_data(results_path, constants.DB_WEBSITE,
-                                        args.database, "sql")
-    elif args.filename is not None:
-        version_filepath = None
-        db_filepath = args.filename
-    else:
-        version_filepath = None
-        db_filename = None
-        db_filepath = None
-
-    if args.install == True:
-        install_db(db_filepath, args, schema_version=schema_version)
-
-    if args.remove == True:
-        if results_path is not None:
-            remove_data(results_path)
-
-
-
-
-def install_db(db_filepath, args, schema_version=None):
-    """Install database."""
-    if args.new == True:
-        result1 = [True, None]
-    else:
-        result1 = basic.verify_path2(db_filepath, kind="file", expect=True)
-    if result1[0] == True:
-        engine1, msg = mysqldb.get_engine(database="", echo=False)
-        if engine1 is not None:
-            result2 = mysqldb.drop_create_db(engine1, args.database)
-            if result2 == 0:
-                engine2, msg = mysqldb.get_engine(
-                                    database=args.database,
-                                    username=engine1.url.username,
-                                    password=engine1.url.password,
-                                    echo=False)
-                if engine2 is not None:
-                    if args.new == True:
-                        mysqldb.execute_transaction(engine2, db_schema_0.STATEMENTS)
-                        convert_args = ["pdm_utils.run",
-                                        "convert",
-                                        args.database,
-                                        "-s",
-                                        str(schema_version)]
-                        convert_db.main(convert_args, engine2)
-                    else:
-                        mysqldb.install_db(engine2, db_filepath)
-                    # Close up all connections in the connection pool.
-                    engine2.dispose()
-                else:
-                    print(f"No connection to the {args.database} database due "
-                          "to invalid credentials or database.")
-            else:
-                print("Unable to create new, empty database.")
-            # Close up all connections in the connection pool.
-            engine1.dispose()
         else:
-            print("Invalid MySQL credentials.")
+            version_filepath, status1 = get_data(results_path, constants.DB_WEBSITE,
+                                                 args.database, "version")
+            db_filepath, status2 = get_data(results_path, constants.DB_WEBSITE,
+                                            args.database, "sql")
+        if (status1 == False or status2 == False):
+            print("Unable to download data from server.")
+            sys.exit(1)
+
+    # If downloading from server, user may have selected to not
+    # install the database file.
+    if install == True:
+        install_db(database, db_filepath=db_filepath, schema_version=schema_version)
+
+    # The output folder was only created for downloading from server.
+    if option == "server":
+        if remove == True:
+            print("Removing downloaded data.")
+            shutil.rmtree(results_path)
+
+
+# TODO test.
+def install_db(database, db_filepath=None, schema_version=None):
+    """Install database."""
+    engine1, msg = mysqldb.get_engine(database="", echo=False)
+    if engine1 is None:
+        print("Invalid MySQL credentials.")
     else:
-        print("Unable to locate database file for installation.")
-        print(result1[1])
+        result2 = mysqldb.drop_create_db(engine1, database)
+        if result2 != 0:
+            print("Unable to create new, empty database.")
+        else:
+            engine2, msg = mysqldb.get_engine(database=database,
+                                              username=engine1.url.username,
+                                              password=engine1.url.password,
+                                              echo=False)
+            if engine2 is None:
+                print(f"No connection to the {database} database due "
+                      "to invalid credentials or database.")
+            else:
+                if db_filepath is not None:
+                    mysqldb.install_db(engine2, db_filepath)
+                else:
+                    mysqldb.execute_transaction(engine2, db_schema_0.STATEMENTS)
+                    convert_args = ["pdm_utils.run", "convert", database,
+                                    "-s", str(schema_version)]
+                    convert_db.main(convert_args, engine2)
+                # Close up all connections in the connection pool.
+                engine2.dispose()
+        # Close up all connections in the connection pool.
+        engine1.dispose()
 
-
-def remove_data(dir):
-    """Remove the folder of downloaded files."""
-    print("Removing downloaded data.")
-    shutil.rmtree(dir)
-
-
+# TODO test.
 def get_data(local_folder, url_folder, db_name, extension):
     """."""
     filename = ".".join([db_name, extension])
@@ -138,6 +112,7 @@ def get_data(local_folder, url_folder, db_name, extension):
     local_path = pathlib.Path(local_folder, filename)
     if local_path.exists() == True:
         print(f"The file {filename} already exists.")
+        status = False
     else:
         status = get_file(url_path, local_path)
     return local_path, status
@@ -147,9 +122,11 @@ def get_data(local_folder, url_folder, db_name, extension):
 # a file will still be created with text indicating
 # there was an error. So this step needs to catch that
 # error.
+# TODO test.
 def get_file(file_url, filepath):
     """Retrieve a file from the server."""
     print(f"Downloading {filepath.name} file.")
+    # Command line structure: curl website > output_file
     command_string = f"curl {file_url}"
     command_list = command_string.split(" ")
     status = False
@@ -162,84 +139,50 @@ def get_file(file_url, filepath):
     return status
 
 
-# TODO unittest.
+# TODO test.
 def parse_args(unparsed_args_list):
     """Verify the correct arguments are selected for getting a new database."""
 
-    UPDATE_DB_HELP = ("Pipeline to retrieve and install a new version of "
-                   "a MySQL database.")
-    DATABASE_HELP = "Name of the MySQL database."
-    OUTPUT_FOLDER_HELP = ("Path to the folder to create the folder for "
-                          "downloading the database.")
-    INSTALL_HELP = \
-        ("Indicates if the new version should be installed.")
-    DOWNLOAD_HELP = \
-        ("Indicates if the new version should be downloaded.")
-    FORCE_INSTALL_HELP = \
-        ("Indicates if the downloaded version should overwrite the existing "
-         "database or create a new database if it is not already present.")
-    REMOVE_HELP = \
-        ("Indicates if the downloaded file should be removed after installation.")
-    ALL_HELP = \
-        ("Indicates if the entire pipeline should run: "
-         "download, install, and then remove the temp files.")
+    GET_DB_HELP = \
+        "Pipeline to retrieve and install a new version of a MySQL database."
+    DATABASE_HELP = \
+        "Name of the MySQL database."
+    OPTION_HELP = \
+        "Source of data to create database."
+    SERVER_HELP = \
+        "Download database from server."
+    OUTPUT_FOLDER_HELP = \
+        ("Path to the folder to create the folder for downloading "
+        f"the database. Default is {DEFAULT_OUTPUT_FOLDER}")
+    DOWNLOAD_ONLY_HELP = \
+        "The database should be downloaded but not installed locally."
     FILENAME_HELP = \
-        ("Indicates the name of the SQL file and version file. "
-         "By default, the filename is "
-         "assumed to be the same as the database name. This option enables "
-         "database to be created from a file of a different name.")
+        "Name of the SQL file and version file."
     NEW_HELP = \
-        ("Indicates whether a new, empty database with the most current schema "
-         "should be created. This option overrides all other options.")
+        "Indicates whether a new, empty database should be created."
+    SCHEMA_VERSION_HELP = \
+        "Database schema version to which the database should be converted."
 
-    parser = argparse.ArgumentParser(description=UPDATE_DB_HELP)
+    parser = argparse.ArgumentParser(description=GET_DB_HELP)
     parser.add_argument("database", type=str, help=DATABASE_HELP)
-    parser.add_argument("-o", "--output_folder", type=pathlib.Path,
+    subparsers = parser.add_subparsers(dest="option", help=OPTION_HELP)
+
+    parser_a = subparsers.add_parser("server", help=SERVER_HELP)
+    parser_a.add_argument("-o", "--output_folder", type=pathlib.Path,
         default=pathlib.Path(DEFAULT_OUTPUT_FOLDER), help=OUTPUT_FOLDER_HELP)
-    parser.add_argument("-d", "--download", action="store_true",
-        default=False, help=DOWNLOAD_HELP)
-    parser.add_argument("-i", "--install", action="store_true",
-        default=False, help=INSTALL_HELP)
-    parser.add_argument("-r", "--remove", action="store_true",
-        default=False, help=REMOVE_HELP)
-    parser.add_argument("-a", "--all_steps", action="store_true",
-        default=False, help=ALL_HELP)
-    parser.add_argument("-f", "--filename", type=pathlib.Path,
-        help=FILENAME_HELP)
-    parser.add_argument("-n", "--new", action="store_true",
-        default=False, help=NEW_HELP)
+    parser_a.add_argument("-d", "--download_only", action="store_true",
+        default=False, help=DOWNLOAD_ONLY_HELP)
+
+    parser_b = subparsers.add_parser("file", help="Install from local file.")
+    parser_b.add_argument("filename", type=pathlib.Path, help=FILENAME_HELP)
+
+    parser_c = subparsers.add_parser("new", help="Create empty database.")
+    parser_c.add_argument("-s", "--schema_version", type=int,
+        choices=list(convert_db.CHOICES), default=convert_db.CURRENT_VERSION,
+        help=SCHEMA_VERSION_HELP)
 
     # Assumed command line arg structure:
     # python3 -m pdm_utils.run <pipeline> <additional args...>
     # sys.argv:      [0]            [1]         [2...]
     args = parser.parse_args(unparsed_args_list[2:])
-
-    if args.new == True:
-        args.all_steps == False
-        args.download = False
-        args.install = True
-        args.remove = False
-    elif args.all_steps == True:
-        args.download = True
-        args.install = True
-        args.remove = True
-    else:
-        pass
     return args
-
-
-# TODO unittest.
-def choose_schema_version():
-    prompt1 = "Do you want the current database schema? "
-    response1 = basic.ask_yes_no(prompt=prompt1, response_attempt=3)
-    if response1 is not None:
-        if response1 == False:
-            prompt2 = ("Select the schema version: "
-                       f"\n{convert_db.VERSIONS} ")
-            schema_version = basic.select_option(prompt2, convert_db.VERSIONS)
-        else:
-            schema_version = convert_db.CURRENT_VERSION
-    else:
-        print("The database will not be created.")
-        sys.exit(1)
-    return schema_version
