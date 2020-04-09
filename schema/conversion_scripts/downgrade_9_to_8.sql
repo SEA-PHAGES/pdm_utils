4f6dc1051c5d21e1e3a346f74a28a56645c3599d
Travis Mavrich <trm53@pitt.edu>
2020-04-09 12:20:27 -0400

    developing schema 9 conversion scripts

--- schema/conversion_scripts/downgrade_9_to_8.sql
+++ schema/conversion_scripts/downgrade_9_to_8.sql
@@ -1,24 +1,27 @@
 # MySQL script to downgrade the database schema from version 9 to 8.
-# Add column `Sequence` to `trna`;
-ALTER TABLE `trna` ADD COLUMN `Sequence` varchar(100) NOT NULL;     # Note: Artificially restricts sequence length - may truncate some
-ALTER TABLE `trna` ADD COLUMN `Product` blob;                       # Note: Will be totally empty and may not reflect the flat file
-ALTER TABLE `trna` ADD COLUMN `InfernalScore` decimal(4,2);         # Note: Will be totally empty and thus not reflect reality
-ALTER TABLE `trna` DROP `Source`;                                   # Note: Will lose track of which program(s) support this trna
-
+ALTER TABLE `trna` DROP COLUMN `Source`;
+ALTER TABLE `trna` ADD COLUMN `InfernalScore` decimal(4,2) DEFAULT NULL AFTER `Anticodon`;
+ALTER TABLE `trna` ADD COLUMN `Product` blob AFTER `Orientation`;
+ALTER TABLE `trna` ADD COLUMN `Sequence` varchar(100) DEFAULT NULL AFTER `Orientation`;
 UPDATE `trna`
   LEFT JOIN `phage` ON `trna`.`PhageID` = `phage`.`PhageID`
-  SET `trna`.`Sequence` = SUBSTRING(`phage`.`Sequence` FROM `trna`.`Start` FOR `trna`.`Length`);   # Populate sequence column
+  SET `trna`.`Sequence` = SUBSTRING(`phage`.`Sequence` FROM `trna`.`Start` FOR `trna`.`Length`);
+ALTER TABLE `trna` MODIFY COLUMN `Sequence` varchar(100) NOT NULL;
+UPDATE `trna` SET `AminoAcid` = 'OTHER' WHERE `AminoAcid` in ('fMet', 'Ile2', 'Pyl', 'Sec', 'Stop');
+ALTER TABLE `trna` MODIFY COLUMN `AminoAcid` enum('Ala','Arg','Asn','Asp','Cys','Gln','Glu','Gly','His','Ile','Leu','Lys','Met','Phe','Pro','Ser','Thr','Trp','Tyr','Val','Undet','OTHER') NOT NULL;
 
 CREATE TABLE `trna_structures` (
   `Sequence` varchar(100) NOT NULL,
   `Structure` varchar(300) NOT NULL,
   PRIMARY KEY (`Sequence`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+ALTER TABLE `trna` MODIFY COLUMN `Structure` varchar(300) NOT NULL;
+INSERT IGNORE INTO `trna_structures` SELECT `Sequence`, `Structure` FROM `trna`;
+ALTER TABLE `trna` DROP COLUMN `Structure`;
 
-INSERT IGNORE INTO `trna_structures` SELECT `Sequence`, `Structure` FROM `trna`;    # Should copy sequences and structures to `trna_structures`
-ALTER TABLE `trna` DROP `Structure`;                                                # Can now delete structure from `trna`
-
-# Modify the set of allowable amino acids - 20 standard plus Undet, OTHER
-# NOTE: Anything that was previously non-standard and non-'OTHER' will be 'OTHER' - may not be totally accurate
-UPDATE `trna` SET `AminoAcid` = 'OTHER' WHERE `AminoAcid` in ('fMet', 'Ile2', 'Pyl', 'Sec', 'Stop');
-ALTER TABLE `trna` MODIFY COLUMN `AminoAcid` enum('Ala','Arg','Asn','Asp','Cys','Gln','Glu','Gly','His','Ile','Leu','Lys','Met','Phe','Pro','Ser','Thr','Trp','Tyr','Val','Undet','OTHER') NOT NULL;
+### DATA_LOSS_SUMMARY
+# INACCURATE_COLUMN:trna.Product
+# INACCURATE_COLUMN:trna_structures.Sequence
+# INACCURATE_COLUMN:trna.InfernalScore
+# INACCURATE_COLUMN:trna.AminoAcid
+# LOST_COLUMN:trna.Source
--- schema/conversion_scripts/upgrade_8_to_9.sql
+++ schema/conversion_scripts/upgrade_8_to_9.sql
@@ -1,22 +1,21 @@
 # MySQL script to upgrade the database schema from version 8 to 9.
-ALTER TABLE `trna` ADD COLUMN `Structure` blob DEFAULT NULL;
-
-# Copy from trna_structures.Structure to trna.Structure, then delete now-defunct trna_structures
+ALTER TABLE `trna` ADD COLUMN `Structure` varchar(300) NOT NULL;
 UPDATE `trna`
   LEFT JOIN `trna_structures` ON `trna`.`Sequence` = `trna_structures`.`Sequence`
   SET `trna`.`Structure` = `trna_structures`.`Structure`;
+ALTER TABLE `trna` MODIFY COLUMN `Structure` blob DEFAULT NULL;
 DROP TABLE `trna_structures`;
-
-# Modify the set of allowable amino acids - 20 standard plus SeC, Pyl, generic Stop, fMet, Ile2, OTHER
-# NOTE: Anything that was previously 'Undet' will be 'OTHER'; anything that was 'OTHER' may not be as specific as would now be allowed
 UPDATE `trna` SET `AminoAcid` = 'OTHER' WHERE `AminoAcid` = 'Undet';
 ALTER TABLE `trna` MODIFY COLUMN `AminoAcid` enum('Ala','Arg','Asn','Asp','Cys','fMet','Gln','Glu','Gly','His','Ile','Ile2','Leu','Lys','Met','Phe','Pro','Pyl','SeC','Ser','Thr','Trp','Tyr','Val','Stop','OTHER') NOT NULL;
-
-# Delete sequence, product, and Infernal columns because they are unneeded, redundant, and non-universal (respectively)
-ALTER TABLE `trna` DROP `Sequence`;
-ALTER TABLE `trna` DROP `Product`;
-ALTER TABLE `trna` DROP `InfernalScore`;
-
-# Add a column to store which program(s) predict the annotated tRNA
-# NOTE: This column will have all NULL values after upgrade
+ALTER TABLE `trna` DROP COLUMN `Sequence`;
+ALTER TABLE `trna` DROP COLUMN `Product`;
+ALTER TABLE `trna` DROP COLUMN `InfernalScore`;
 ALTER TABLE `trna` ADD COLUMN `Source` enum('aragorn', 'trnascan', 'both') DEFAULT NULL;
+
+
+### DATA_LOSS_SUMMARY
+# LOST_TABLE:trna_structures
+# LOST_COLUMN:trna.Sequence
+# LOST_COLUMN:trna.Product
+# LOST_COLUMN:trna.InfernalScore
+# INACCURATE_COLUMN:trna.Source
