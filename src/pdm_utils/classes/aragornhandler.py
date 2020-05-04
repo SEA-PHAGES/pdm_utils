257e8339cdee11236b65aa7c398c943380c606cc
chg60 <chg60@pitt.edu>
2020-05-04 06:57:42 -0400

    fixed regex typo

--- src/pdm_utils/classes/aragornhandler.py
+++ src/pdm_utils/classes/aragornhandler.py
@@ -78,7 +78,7 @@ class AragornHandler:
         """
         # values:        orient,  start, stop,            peptide tag
         # indices:          0       1     2                    3
-        re_str = "tmRNA\s+(\w+)?\[(\d+),(\d+)\]\s+\d+,\d+\s+([\w*]*)"
+        re_str = "tmRNA\s+(\w+)?\[(\d+),(\d+)\]\s+\d+,\d+\s+([\w|*]*)"
         regex = re.compile(re_str, re.MULTILINE | re.DOTALL)
 
         tmrnas = regex.findall(self.out_str)
