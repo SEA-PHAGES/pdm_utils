41abaedf6fffaca82708fbc73523594657ec7a2a
Lawrence Abad <laa89@pitt.edu>
2020-07-05 15:47:09 -0400

    Pham eval unit unittests

--- src/pdm_utils/functions/pham_evaluation.py
+++ src/pdm_utils/functions/pham_evaluation.py
@@ -1,11 +1,35 @@
 import shlex
+import textwrap
 from subprocess import Popen, PIPE
 
+#Imports to be removed
+import sys
+import shutil
+from pathlib import Path
 from pdm_utils.classes.alchemyhandler import AlchemyHandler
-from pdm_utils.classes.clustal import *
+from pdm_utils.classes.filter import Filter
+from statistics import mean
+
+from Bio.Emboss import Applications
+from Bio import AlignIO
+
+from pdm_utils.classes import clustal
 from pdm_utils.functions import mysqldb_basic
 
+#GLOBAL VARIABLES
+#----------------------------------------------------------------------
+EMBOSS_TOOLS = ["needle", "water", "stretcher"]
 
+EMBOSS_TOOL_SETTINGS = {"needle"    : {"gapopen"    : 10,
+                                       "gapextend"  : 0.5},
+                        "stretcher" : {"gapopen"    : 12,
+                                       "gapextend"  : 2},
+                        "water"     : {"gapopen"    : 10,
+                                       "gapextend"  : 0.5}
+                       }
+
+#FILE I/O TOOLS
+#----------------------------------------------------------------------
 def get_pham_genes(engine, phamid):
     """
     Queries the database for the geneids and translations found in the
@@ -15,7 +39,7 @@ def get_pham_genes(engine, phamid):
     :param engine: the Engine allowing access to the database
     :type engine: sqlalchemy Engine
     :param phamid: the pham whose genes are to be returned
-    :type phamid: int
+    :type phamid: intbiopython pairwise distance
     :return: pham_genes
     :rtype: dict
     """
@@ -34,23 +58,128 @@ def get_pham_genes(engine, phamid):
 
     return pham_genes
 
-
-def write_genes_to_fasta(genes, infile):
+def write_genes_to_fasta(genes_translations, infile):
     """
     Writes the input genes to the indicated file in FASTA multiple
     sequence format (unaligned).
-    :param genes: the genes to be written to file
+    :param genes_translations: the genes and translations to be written to file 
     :type genes: dict
-    :param infile: the name of the file to write the genes to
+    :param infile: the path of the file to write the genes to
+    :type infile: Path
     :type infile: str
     :return:
     """
-    with open(infile, "w") as fh:
-        for geneid, translation in genes.items():
-            fh.write(f">{geneid}\n{translation}\n")
+    if isinstance(infile, str):
+        file_handle = open(infile, "w")
+    elif isinstance(infile, Path):
+        file_handle = infile.open(mode="w")
+    else:
+        raise TypeError("File path type not supported.")
 
+    for geneid, translation in genes_translations.items():
+        wrapped_translation = textwrap.wrap(annotations, 60)
+        translation = "\n".join(wrapped_translation)
+        file_handle.write(f">{geneid}\n{translation}\n")
 
-def clustalo(fasta_file, aln_out=None, mat_out=None, threads=1, verbose=0):
+    file_handle.close()
+
+#PAIRWISE ALIGNMENT TOOLS
+#---------------------------------------------------------------------
+def pairwise_align(query_seq_path, target_seq_path, outfile_path, 
+                                                        tool="needle",
+                                                        gapopen=None,
+                                                        gapextend=None):
+    """Aligns two sequences from fasta_files using EMBOSS tools.
+    :param query_seq_path: Path to sequence A for pairwise alignment.
+    :type query_seq_path: Path
+    :param target_seq_path: Path to sequence B for pairwise alignment.
+    :type target_seq_path: Path
+    :param outfile_path: Path to write the alignment.
+    :type outfile_path: Path
+    :returns: Returns a Biopython object containing sequence alignment info.
+    :rtype: MultipleSequenceAlignment
+    """
+    settings = EMBOSS_TOOL_SETTINGS.get(tool)
+    if settings is None:
+        raise ValueError
+
+    if gapopen is None:
+        gapopen = settings["gapopen"]
+    if gapextend is None:
+        gapextend = settings["gapextend"]
+
+    if tool == "needle":
+        cline_init = create_needle_cline
+    elif tool == "stretcher":
+        cline_init = create_stretcher_cline
+    elif tool == "water":
+        cline_init = create_water_cline
+
+    emboss_cline = cline_init(query_seq_path, target_seq_path, outfile_path,
+                                                         gapopen, gapextend)
+
+    stdout, stderr = emboss_cline()
+
+    alignment = AlignIO.read(outfile_path, "emboss") 
+    return alignment
+
+def create_needle_cline(query_seq_path, target_seq_path, outfile_path,
+                                                         gapopen, gapextend):
+    """Helper function that retrieves the Needle cline command.
+    :param query_seq_path: Path to sequence A for pairwise alignment.
+    :type query_seq_path: Path
+    :param target_seq_path: Path to sequence B for pairwise alignment.
+    :type target_seq_path: Path
+    :param outfile_path: Path to write the alignment.
+    :type outfile_path: Path
+    """
+    needle_cline = Applications.NeedleCommandline(
+                                        asequence=query_seq_path,
+                                        bsequence=target_seq_path,
+                                        outfile=outfile_path,
+                                        gapopen=gapopen, gapextend=gapextend)
+
+    return needle_cline
+
+def create_stretcher_cline(query_seq_path, target_seq_path, outfile_path,
+                                                         gapopen, gapextend):
+    """Helper function that retrieves the Needle cline command.
+    :param query_seq_path: Path to sequence A for pairwise alignment.
+    :type query_seq_path: Path
+    :param target_seq_path: Path to sequence B for pairwise alignment.
+    :type target_seq_path: Path
+    :param outfile_path: Path to write the alignment.
+    :type outfile_path: Path
+    """
+    stretcher_cline = Applications.StretcherCommandline(
+                                        asequence=query_seq_path,
+                                        bsequence=target_seq_path,
+                                        outfile=outfile_path,
+                                        gapopen=gapopen, gapextend=gapextend)
+
+    return stretcher_cline
+
+def create_water_cline(query_seq_path, target_seq_path, outfile_path,
+                                                        gapopen, gapextend):
+    """Helper function that retrieves the Needle cline command.
+    :param query_seq_path: Path to sequence A for pairwise alignment.
+    :type query_seq_path: Path
+    :param target_seq_path: Path to sequence B for pairwise alignment.
+    :type target_seq_path: Path
+    :param outfile_path: Path to write the alignment.
+    :type outfile_path: Path
+    """
+    water_cline = Applications.WaterCommandline(
+                                        asequence=query_seq_path,
+                                        bsequence=target_seq_path,
+                                        outfile=outfile_path,
+                                        gapopen=gapopen, gapextend=gapextend)
+
+    return water_cline
+
+#MSA TOOLS
+#---------------------------------------------------------------------
+def clustalo(fasta_file, aln_out_path, mat_out_path, threads=1, verbose=0):
     """
     Runs Clustal Omega to generate a multiple sequence alignment (MSA)
     and percent identity matrix (PIM) for the indicated file. Infile is
@@ -69,13 +198,6 @@ def clustalo(fasta_file, aln_out=None, mat_out=None, threads=1, verbose=0):
     :return: [aln_out, mat_out]
     :rtype: list
     """
-    # Create sensible output filenames if none are provided
-    basename = fasta_file.split(".")[0]
-    if aln_out is None:
-        aln_out = f"{basename}.aln"
-    if mat_out is None:
-        mat_out = f"{basename}.mat"
-
     # Make sure verbose is in range(0,3)
     if verbose <= 0:
         verbose = 0
@@ -84,8 +206,8 @@ def clustalo(fasta_file, aln_out=None, mat_out=None, threads=1, verbose=0):
 
     # Build Clustal Omega command that will produce a clustal-formatted
     # alignment output file and percent identity matrix
-    command = f"clustalo -i {fasta_file} -o {aln_out} --distmat-out=" \
-              f"{mat_out} --outfmt=clustal --full --percent-id --force " \
+    command = f"clustalo -i {fasta_file} -o {aln_out_path} --distmat-out=" \
+              f"{mat_out_path} --outfmt=clustal --full --percent-id --force " \
               f"--output-order=tree-order --threads={threads}"
     for x in range(verbose):
         command += " -v"                # Add verbosity to command
@@ -99,47 +221,5 @@ def clustalo(fasta_file, aln_out=None, mat_out=None, threads=1, verbose=0):
         if verbose > 1 and errors:
             print(errors.decode("utf-8"))
 
-    # Return output files so user can find them easily, whether they specified
-    # the filenames or not
-    return [aln_out, mat_out]
-
-
-#################
-# Example Usage #
-#################
-def main():
-    # Set up MySQL handler
-    alchemist = AlchemyHandler(database="Actinobacteriophage")
-    alchemist.connect(pipeline=True)
-    engine = alchemist.engine
-
-    # Choose a pham, set up infile name, and construct FASTA
-    pham = 49991
-    infile = "/tmp/pham_49991.fasta"
-    genes = get_pham_genes(engine, pham)
-    write_genes_to_fasta(genes, infile)
-
-    # Run clustal and expand the return value to output filenames
-    out_aln, out_mat = clustalo(infile)
-
-    # Parse the identity matrix, get the centroid name and average
-    # distance from centroid to each gene in the pham
-    mat = PercentIdentityMatrix(out_mat)
-    mat.parse_matrix()
-    centroid = mat.get_centroid()
-    average_dist = round(100 - mat.get_average_identity(centroid), 3)
-
-    # Parse the multiple sequence alignment and get centroid sequence w/o gaps
-    aln = MultipleSequenceAlignment(out_aln)
-    aln.parse_alignment()
-    centroid_seq = aln.get_sequence(centroid, gaps=False)
-
-    print("Summary")
-    print("=======================================")
-    print(f"Centroid gene: {centroid}")
-    print(f"Centroid sequence: {centroid_seq[:16]}...")
-    print(f"Average distance from centroid: {average_dist}%")
-
-
 if __name__ == "__main__":
-    main()
+    pass
