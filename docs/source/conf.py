e3dc1f442c491932c205407e3e24808da25e4ce6
Christian Gauthier <chg60@pitt.edu>
2020-10-01 13:10:52 -0400

    incremented package version to 0.8.14

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,9 +20,9 @@ sys.path.insert(0, os.path.abspath('../../src/'))
 project = 'pdm_utils'
 copyright = '2019, Travis Mavrich'
 author = 'Travis Mavrich'
-version = '0.8.13'
+version = '0.8.14'
 # The full version, including alpha/beta/rc tags
-release = '0.8.13'
+release = '0.8.14'
 
 
 # -- General configuration ---------------------------------------------------
