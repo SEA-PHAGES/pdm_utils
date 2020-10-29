0b6b904b486bc58a9167a170544d825bb8f79025
Lawrence Abad <laa89@pitt.edu>
2020-10-29 12:19:57 -0400

    incremented package version to 0.8.17

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,9 +20,9 @@ sys.path.insert(0, os.path.abspath('../../src/'))
 project = 'pdm_utils'
 copyright = '2019, Travis Mavrich'
 author = 'Travis Mavrich'
-version = '0.8.16'
+version = '0.8.17'
 # The full version, including alpha/beta/rc tags
-release = '0.8.16'
+release = '0.8.17'
 
 
 # -- General configuration ---------------------------------------------------
