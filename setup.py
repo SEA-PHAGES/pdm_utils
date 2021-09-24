f2a7115f63bb0b3fe3845d320a16a33a87a637e2
Christian Gauthier <chg60@pitt.edu>
2021-09-23 21:19:36 -0400

    incremented package version to 0.9.16

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pdm_utils",
-    version="0.9.15",
+    version="0.9.16",
     author="Christian Gauthier",
     author_email="chg60@pitt.edu",
     description="MySQL phage genomics database management utilities",
