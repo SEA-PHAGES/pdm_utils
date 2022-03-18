ebc0e74eec958cdfec86bd85ad25459a71a9aba6
Christian <chg60@pitt.edu>
2022-03-18 16:58:19 -0400

    incremented package version to 0.9.20

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,9 +20,9 @@ sys.path.insert(0, os.path.abspath('../../src/'))
 project = 'pdm_utils'
 copyright = '2019, Travis Mavrich'
 author = 'Travis Mavrich'
-version = '0.9.19'
+version = '0.9.20'
 # The full version, including alpha/beta/rc tags
-release = '0.9.19'
+release = '0.9.20'
 
 
 # -- General configuration ---------------------------------------------------
