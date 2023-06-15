fa812ea71ac8b7852c9a232154c01f8471c50aee
Lawrence Abad <laa89@pitt.edu>
2023-06-14 20:29:30 -0400

    Added ReadTheDocs information for schema version 11

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,9 +20,9 @@ sys.path.insert(0, os.path.abspath('../../src/'))
 project = 'pdm_utils'
 copyright = '2019, Travis Mavrich'
 author = 'Travis Mavrich'
-version = '0.9.15'
+version = '1.0.0'
 # The full version, including alpha/beta/rc tags
-release = '0.9.15'
+release = '1.0.0'
 
 
 # -- General configuration ---------------------------------------------------
--- src/pdm_utils/__init__.py
+++ src/pdm_utils/__init__.py
@@ -2,4 +2,4 @@ from pdm_utils.classes.alchemyhandler import AlchemyHandler
 from pdm_utils.classes.filter import Filter
 
 name = "pdm_utils"
-__version__ = "0.9.15"
+__version__ = "1.0.0"
